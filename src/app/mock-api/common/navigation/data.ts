ec3bc34bb5642534e07b738f8d7f97d5caa87a5e
Mohit-S-Rane <mohitsrane16@gmail.com>
2022-12-20 12:43:34 +0530

    initial project commit

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,39 @@
+/* tslint:disable:max-line-length */
+import { MsrNavigationItem } from '@msr/components/navigation';
+
+export const defaultNavigation: MsrNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const compactNavigation: MsrNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const futuristicNavigation: MsrNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const horizontalNavigation: MsrNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
